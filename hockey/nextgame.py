26b3c7f615596a072e4abb4d417aee9fb61b1557
Colin Campbell <colin@elusivelabs.com>
2021-03-24 08:31:10 -0400

    Check n games

--- hockey/nextgame.py
+++ hockey/nextgame.py
@@ -9,34 +9,40 @@ def leafsid():
 def seasonend():
     return "2021-05-21"
 
-def nextgame():
+def nextgame(num=1):
     url = "https://statsapi.web.nhl.com/api/v1/schedule?teamId={}&startDate={}&endDate={}"
+    nextgame = [] 
+
     resp = requests.get(url.format(leafsid(), date.today(), seasonend()))
     data = resp.json()
-    nextgame = data["dates"][0]["games"][0]
+
+    for i in range(num):
+        nextgame.append(data["dates"][i]["games"][0])
+
     return nextgame
 
 def main():
+
     ng = nextgame()
-    teams =  ng["teams"]
-    home = teams["home"]
-    away = teams["away"]
+    for game in nextgame(1):
+        teams =  game["teams"]
+        home = teams["home"]
+        away = teams["away"]
+        gamedate = game["gameDate"]
 
-    gamedate = ng["gameDate"]
-
-    gameinfo = "{} ({}-{}-{}) @ {} ({}-{}-{}) {}"
+        gameinfo = "{} ({}-{}-{}) @ {} ({}-{}-{}) {}"
     
-    print(gameinfo.format(
-        away["team"]["name"], 
-        away["leagueRecord"]["wins"],
-        away["leagueRecord"]["losses"],
-        away["leagueRecord"]["ot"],
-        home["team"]["name"],
-        home["leagueRecord"]["wins"],
-        home["leagueRecord"]["losses"],
-        home["leagueRecord"]["ot"],
-        gamedate,
-    ))
+        print(gameinfo.format(
+            away["team"]["name"], 
+            away["leagueRecord"]["wins"],
+            away["leagueRecord"]["losses"],
+            away["leagueRecord"]["ot"],
+            home["team"]["name"],
+            home["leagueRecord"]["wins"],
+            home["leagueRecord"]["losses"],
+            home["leagueRecord"]["ot"],
+            gamedate,
+        ))
 
 if __name__ == "__main__":
     main()
